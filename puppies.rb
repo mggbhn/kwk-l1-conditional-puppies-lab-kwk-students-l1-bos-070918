c090adc2c12a761bc7801c7a9a7da9aa954df525
mggbhn <maggiebswim@gmail.com>
2018-07-10 19:13:21 +0000

    automatically backed up by learn

--- puppies.rb
+++ puppies.rb
@@ -2,19 +2,33 @@
 
 def no_puppies(x)
   if x > 0 
-    "No more puppies"
+    "Get back to your animal goals!"
   else
-    "Well done, Rachel!"
+    "Good job, Rachel!"
   end
 end
 
 # 2. Rachel's animal accumulation has been going well and we're going to cut her some slack. Write a new method called less_puppies that takes in a number of puppies as an argument and tells Rachel good job when she gets less than 3 puppies, to slow down if she's found exactly 3 puppies, and to get back on her animal restriction if she gets more than 3 puppies.
 
-
+def less_puppies(x)
+  if x = 3 
+    "Slow down! No more puppies"
+  elsif x < 3
+    "Good job, Rachel!"
+  else
+    ""
+  end
+end
 
 # 3. We've decided to give Rachel a little more control over her puppy obsession. Write a method called some_puppies that takes in two arguments - the number of puppies she has found and a maximum puppy allowance. This method should tell Rachel good job if she gets less than half her maximum allowance and to get back on her puppy restriction when she exceeds her maximum.
 
-
+def some_puppies(x,y)
+  if x > 0 
+    "Get back to your animal goals!"
+  else
+    "Good job, Rachel!"
+  end
+end
 
 # BONUS
 # 4. Rachel has decided to track both her puppy allowance AND her cat allowance. Write a method called new_animal accumulation that takes in two arguments - number of puppies and number of cats. As long as she gets zero puppies OR zero cats this method should return "Good job!". If she gets 1 or more puppies AND 1 or more cats it should tell her she should really slow back down!
