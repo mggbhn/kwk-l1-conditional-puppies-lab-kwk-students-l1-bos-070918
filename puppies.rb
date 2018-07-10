effafca7d4fb8a37f12bf959a3693c9ddac9c7f5
mggbhn <maggiebswim@gmail.com>
2018-07-10 19:26:00 +0000

    automatically backed up by learn

--- puppies.rb
+++ puppies.rb
@@ -32,3 +32,16 @@ end
 
 # BONUS
 # 4. Rachel has decided to track both her puppy allowance AND her cat allowance. Write a method called new_animal accumulation that takes in two arguments - number of puppies and number of cats. As long as she gets zero puppies OR zero cats this method should return "Good job!". If she gets 1 or more puppies AND 1 or more cats it should tell her she should really slow back down!
+
+def new_animal(x,y)
+  if x == 0 || y == 0
+    "Good job!"
+  else 
+    "Get back to your animal goals!"
+  end
+end
+
+
+
+
+
